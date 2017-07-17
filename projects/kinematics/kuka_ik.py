83416e6b38f79b9a8354e74cbcbb8ccf9c19983b
Josh Schertz <joshschertz3@gmail.com>
2017-07-17 01:07:28 -0400

    Update kuka_ik.py code to get correct thetas

--- projects/kinematics/kuka_ik.py
+++ projects/kinematics/kuka_ik.py
@@ -1,6 +1,7 @@
 import numpy as np
-from sympy import symbols, pi, sin, cos, acos, atan2, sqrt, simplify
+from sympy import symbols, pi, sin, cos, asin, atan2, sqrt, simplify
 from sympy.matrices import Matrix
+import tf
 
 """
 Test file for building the Kuka 6 DoF manipulator's forward and inverse
@@ -64,11 +65,11 @@ a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')    # link lengths
 
 # Modified DH params for KUKA KR210
 s = {alpha0:     0, d1:  0.75, a0:      0,
-     alpha1: -pi/2, d2:     0, a1:   0.35, theta2: (theta2 - pi/2),
+     alpha1:  pi/2, d2:     0, a1:   0.35, theta2: (theta2 - pi/2),
      alpha2:     0, d3:     0, a2:   1.25,
-     alpha3: -pi/2, d4:  1.50, a3: -0.054,
-     alpha4:  pi/2, d5:     0, a4:      0,
-     alpha5: -pi/2, d6:     0, a5:      0,
+     alpha3:  pi/2, d4:  1.50, a3: -0.054,
+     alpha4: -pi/2, d5:     0, a4:      0,
+     alpha5:  pi/2, d6:     0, a5:      0,
      alpha6:     0, d7: 0.303, a6:      0, theta7: 0}
 
 # EE location and orientation
@@ -101,43 +102,49 @@ lz = Rrpy[2, 0]
 wx = px - (s[d7] + s[d6]) * lx
 wy = py - (s[d7] + s[d6]) * ly
 wz = pz - (s[d7] + s[d6]) * lz
-#print(wx, wy, wz)
+#print('EE location: (%s, %s, %s)' % (wx, wy, wz))
 
 ##############################################################################
 # Step 2: Calculate thetas for joint 1, 2 and 3 (determines EE position)
 
+# Determine the angle for joint 1
 theta1 = atan2(wy, wx).evalf()
 theta1 = np.clip(theta1, -185*dtr, 185*dtr)
-print('theta1: %s' % theta1)
 
-# Calculation from graph 1
-#r1 = sqrt(s[a2]**2 + s[d4]**2)
-#r2 = sqrt(sqrt(wx**2 + wy**2)**2 + wz**2)
-#phi1 = atan2(s[a2], s[d4])
-#cos_phi4 = (r2**2 - s[a1]**2 - r1**2) / (-2 * s[a1] * r1)
-#phi4 = atan2(sqrt(1 - cos_phi4**2), cos_phi4)
-#theta3 = (phi4 - phi1 - pi/2).evalf()
-##theta3 = (phi4 - phi1).evalf()
-#print('graph_1 theta3: %s' % theta3)
+# Find the coordinates of x2, y2 and z2 considering theta 1
+x2 = s[a1] * cos(theta1)
+y2 = s[a1] * sin(theta1)
+z2 = s[d1]
 
-X1_3 = wx - s[a2]
-Y1_3 = wy - s[d1]
-Z1_3 = wz - s[d1]
+# Find the x, y and z distances between joint 2 and the wrist center
+X2_WC = wx - x2
+Y2_WC = wy - y2
+Z2_WC = wz - z2
 
-r1 = sqrt(X1_3**2 + Z1_3**2)
-#r1 = sqrt(X1_3**2 + Y1_3**2)
-#r1 = sqrt(sqrt(wx**2 + wz**2) + wy**2)
-cos_phi3 = (r1**2 - s[a2]**2 - s[d4]**2) / (-2 * s[a2] * s[d4])
-phi3 = atan2(sqrt(1 - cos_phi3**2), cos_phi3)
-theta3 = (pi - phi3).evalf()
-print('robogrok theta3: %s' % theta3)
+# Find the distances between joints 2 and 3, and the wrist center
+L2_WC = sqrt(X2_WC**2 + Y2_WC**2 + Z2_WC**2)
+L3_WC = sqrt(s[a3]**2 + s[d4]**2)
 
-cos_phi1 = (s[d4]**2 - s[a2]**2 - r1**2) / (-2 * s[a2] * r1)
-phi1 = atan2(sqrt(1 - cos_phi1**2), cos_phi1)
-phi2 = atan2(Z1_3, X1_3)
-theta2 = phi2 - phi1
-#theta2 = np.clip(theta2, -45*dtr, 85*dtr)
-print('robogrok theta2: %s' % theta2)
+# Determine the angle for joint 3
+phi1 = s[alpha3] - atan2(s[a3], s[d4])
+cos_phi2 = (L2_WC**2 - L3_WC**2 - s[a2]**2) / (2 * L3_WC * s[a2])
+if cos_phi2 > 1:
+    cos_phi2 = 1
+    print('cos_phi2 is greater than 1')
+phi2 = atan2(sqrt(1 - cos_phi2**2), cos_phi2)
+theta3 = (phi2 - phi1).evalf()
+theta3 = np.clip(theta3, -210*dtr, (155-90)*dtr)
+
+# Determine the angle for joint 2
+L2_WC_xy = sqrt(X2_WC**2 + Y2_WC**2)
+phi3 = atan2(Z2_WC, L2_WC_xy)
+cos_phi4 = (L3_WC**2 - L2_WC**2 - s[a2]**2) / (-2 * L2_WC * s[a2])
+if cos_phi4 > 1:
+    cos_phi4 = 1
+    print('cos_phi4 is greater than 1')
+phi4 = atan2(sqrt(1 - cos_phi4**2), cos_phi4)
+theta2 = (s[alpha1] - (phi3 + phi4)).evalf()
+theta2 = np.clip(theta2, -45*dtr, 85*dtr)
 
 ##############################################################################
 # Step 3: Determine the rotation matrix for the spherical wrist joints
@@ -148,30 +155,70 @@ T1_2 = build_mod_dh_matrix(s=s, theta=theta2, alpha=alpha1, d=d2, a=a1)
 T2_3 = build_mod_dh_matrix(s=s, theta=theta3, alpha=alpha2, d=d3, a=a2)
 
 # Rotation matrix of the first three joints
-R0_3 = (T0_1 * T1_2 * T2_3).evalf(subs={theta1: theta1, theta2: theta2,
+R0_3 = (T0_1 * T1_2 * T2_3).evalf(subs={theta1: theta1,
+                                        theta2: theta2,
                                         theta3: theta3})[0:3, 0:3]
 
+# Correction to account for orientation difference between definition of
+#   gripper link in the URDF file and the DH convention.
+#   (rotation around Z axis by 180 deg and X axis by -90 deg)
+R_corr = simplify(rot_z(pi) * rot_y(-pi/2))
+
 # Calculate the symbolic rotation matrix of the spherical wrist joints
-R3_6 = R0_3.inv() * Rrpy
+R3_6 = R0_3.T * Rrpy * R_corr
 
 ##############################################################################
-# Step 4: Calculate the spherical wrist joint angles
+# Step 4: Calculate the spherical wrist joint angles by converting the
+#   the rotation matrix to Euler angles
 
-r31 = R3_6[2, 0]
+## tf requires a numpy matrix instead of a sympy matrix
+#R3_6_np = np.array(R3_6).astype(np.float64)
+#
+## Convert the rotation matrix to Euler angles using tf
+#theta4, theta5, theta6 = tf.transformations.euler_from_matrix(R3_6_np, axes='ryzx')
+#
+#theta4 = np.pi + theta4
+#theta5 = np.pi/2 - theta5
+#theta6 = np.pi/2 + theta6
+
 r11 = R3_6[0, 0]
+r12 = R3_6[0, 1]
+r13 = R3_6[0, 2]
 r21 = R3_6[1, 0]
+r31 = R3_6[2, 0]
 r32 = R3_6[2, 1]
 r33 = R3_6[2, 2]
 
-theta4 = atan2(r32, r33).evalf()
-#theta4 = np.clip(theta4, -350*dtr, 350*dtr)
-
-theta5 = atan2(-r31, sqrt(r11**2 + r21**2)).evalf()
+# Pitch angle; rotation around the y-axis
+#theta5 = atan2(-r31, sqrt(r11**2 + r21**2)).evalf()
+theta5 = asin(r31)
 #theta5 = np.clip(theta5, -125*dtr, 125*dtr)
 
-theta6 = atan2(r21, r11).evalf()
-#theta6 = np.clip(theta6, -350*dtr, 350*dtr)
+if r31 == 1:
+    # Gimbal lock at pitch = -90
+    theta4 = 0                          # yaw = 0
+    theta6 = atan2(-r12, -r13).evalf()  # roll
+    print('Gimbal lock at pitch = -90')
+elif r31 == -1:
+    # Gimal lock at pitch = 90
+    theta4 = 0                          # yaw = 0
+    theta6 = atan2(r12, r13).evalf()    # roll
+    print('Gimbal lock at pitch = 90')
+else:
+    # General orientation
 
-print(theta4)
-print(theta5)
-print(theta6)
+    # Yaw angle; rotation around the z-axis
+    theta4 = (atan2(r21, r11) - np.pi/2).evalf()
+    #theta4 = np.clip(theta4, -350*dtr, 350*dtr)
+    
+    # Roll angle; rotation around the x-axis
+    #theta6 = np.pi/2 + atan2(r32, r33)
+    theta6 = (atan2(r32, r33) - np.pi/2).evalf()
+    #theta6 = np.clip(theta6, -350*dtr, 350*dtr)
+
+print('Theta 1: %s' % theta1)
+print('Theta 2: %s' % theta2)
+print('Theta 3: %s' % theta3)
+print('Theta 4: %s' % theta4)
+print('Theta 5: %s' % theta5)
+print('Theta 6: %s' % theta6)
